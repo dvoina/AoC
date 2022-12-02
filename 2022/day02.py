c2a8d170474404a1a1ab37aa9e5ee8ea1c85fdde
Daniel Voina <dvoina@outlook.com>
2022-12-02 15:33:41 +0200

    Day 2

--- 2022/day02.py
+++ 2022/day02.py
@@ -1,22 +1,14 @@
 #Rock, Paper, Scissors
-s1 = {
-    'X':{'A':4, 'B':1, 'C':7},
-    'Y':{'A':8, 'B':5, 'C':2},
-    'Z':{'A':3, 'B':9, 'C':6}
-}
-
-
-s2 = {
-    'X':{'A':3, 'B':1, 'C':2},
-    'Y':{'A':4, 'B':5, 'C':6},
-    'Z':{'A':8, 'B':9, 'C':7}
+s = {
+    'X':{'A':(4, 3), 'B':(1, 1), 'C':(7, 2)},
+    'Y':{'A':(8, 4), 'B':(5, 5), 'C':(2, 6)},
+    'Z':{'A':(3, 8), 'B':(9, 9), 'C':(6, 7)}
 }
 
 encryptedBook = [tuple(l.strip().split(" ")) for l in open("2022/day02.txt").readlines()]
 
-t1 = 0 
-t2 = 0
+s1, s2 = 0, 0
 for game in encryptedBook:
-    t1 += s1[game[1]][game[0]]
-    t2 += s2[game[1]][game[0]]
-print(t1, t2)
+    s1 += s[game[1]][game[0]][0]
+    s2 += s[game[1]][game[0]][1]
+print(s1, s2)
