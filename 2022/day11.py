2eadcbb7e22a37766a8ffe3a3dbf883b403e15e5
Daniel Voina <dvoina@outlook.com>
2022-12-11 17:21:12 +0200

    Day 11

--- 2022/day11.py
+++ 2022/day11.py
@@ -6,33 +6,40 @@ class Monkey(object):
         self.items = []
         self.operation = ""
         self.test = 0
+        self.div = 3
         self.mt = 0
         self.mf = 0
         self.activity = 0
 
     def ifTrue(self, o, monkeys:dict):
-        print(f"Item with worry level {o} is thrown to monkey {self.mt}.")
+        # print(f"Item with worry level {o} is thrown to monkey {self.mt}.")
         monkeys.get(self.mt).items.append(o)
 
     def ifFalse(self, o, monkeys: dict):
-        print(f"Item with worry level {o} is thrown to monkey {self.mf}.")
+        # print(f"Item with worry level {o} is thrown to monkey {self.mf}.")
         monkeys.get(self.mf).items.append(o)
 
-    def round(self, monkeys:dict, part1):
+    def round(self, monkeys:dict, part1:bool, d=3):
+        self.activity += len(self.items)
         while self.items:
             w = self.items.pop()
-            print(f"Monkey inspects an item with a worry level of {w}.")
+
+            # print(f"Monkey inspects an item with a worry level of {w}.")
             w = self._operation(w)
+            
             if part1:
                 w = w // 3
-            print(f"Monkey gets bored with item. Worry level is divided by 3 to {w}.")
+                # print(f"Monkey gets bored with item. Worry level is divided by 3 to {w}.")
+            else:
+                w = w % d
+
             if w % self.test == 0:
-                print(f"Current worry level is divisible by {self.test}.")
+                # print(f"Current worry level is divisible by {self.test}.")
                 self.ifTrue(w, monkeys)
             else:
-                print(f"Current worry level is not divisible by {self.test}.")
+                # print(f"Current worry level is not divisible by {self.test}.")
                 self.ifFalse(w, monkeys)
-            self.activity += 1
+
 
     def _operation(self, old):
         return eval(self.operation)
@@ -52,33 +59,48 @@ class Monkey(object):
         return str(self)
 
 scripts = [tuple(s.split("\n")) for s in open("2022/day11.txt").read().split("\n\n")]
-monkeys = {}
 
+# part 1
+p = 1
+monkeys = {}
 for s in scripts:
     m = Monkey()
     m.number = int(re.findall("\d+", s[0])[0])
     m.items = [int(x.strip()) for x in s[1].replace("  Starting items: ", "").split(",")]
     m.operation = s[2].replace("  Operation: new = ", "")
     m.test = int(re.findall("\d+", s[3])[0])
+    p *= m.test
     m.mt = int(re.findall("\d+", s[4])[0])
     m.mf = int(re.findall("\d+", s[5])[0])
-    print(m)
+    # print(m)
     monkeys[m.number] = m
-
-# part 1
 for _ in range(20):
     for m in sorted(monkeys.keys()):
         monkey = monkeys.get(m)
         monkey.round(monkeys, True)
 
-top2 = sorted(monkeys.values(),key=lambda x: x.activity,reverse=True)[0:2]
+top2 = sorted(monkeys.values(),key=lambda x: x.activity)[-2:]
 print(top2[0].activity * top2[1].activity)
 
+
 # part 2
-for _ in range(1000):
+p = 1
+monkeys = {}
+for s in scripts:
+    m = Monkey()
+    m.number = int(re.findall("\d+", s[0])[0])
+    m.items = [int(x.strip()) for x in s[1].replace("  Starting items: ", "").split(",")]
+    m.operation = s[2].replace("  Operation: new = ", "")
+    m.test = int(re.findall("\d+", s[3])[0])
+    p *= m.test
+    m.mt = int(re.findall("\d+", s[4])[0])
+    m.mf = int(re.findall("\d+", s[5])[0])
+    # print(m)
+    monkeys[m.number] = m
+for _ in range(10000):
     for m in sorted(monkeys.keys()):
         monkey = monkeys.get(m)
-        monkey.round(monkeys, False)
+        monkey.round(monkeys, False, p)
 
-top2 = sorted(monkeys.values(),key=lambda x: x.activity,reverse=True)[0:2]
+top2 = sorted(monkeys.values(),key=lambda x: x.activity)[-2:]
 print(top2[0].activity * top2[1].activity)
